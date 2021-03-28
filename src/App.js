8ff679e8832a177aeae51b4f63e2785227b5049d
meeya <meeya0724@gmail.com>
2021-03-28 17:51:34 +0900

    hi test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello world
+          Learn React Hi miae
         </a>
       </header>
     </div>
